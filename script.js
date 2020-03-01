bba35432b3c2040babfb191d69b00607a27319c1
prostosite570 <61553765+prostosite570@users.noreply.github.com>
2020-03-01 13:26:16 +0200

    v1

---
+++ script.js
@@ -0,0 +1,59 @@
+$("document").ready(function(){
+	$(".box6-simple-button").click(function(){
+		let x = this.id;
+		if (x == "box6-simple-button1") {
+			document.getElementById("box6-more1").style.display = "block";
+			document.getElementById("box6-more2").style.display = "none";
+			document.getElementById("box6-more3").style.display = "none";
+
+			document.getElementById("box6-simple-content1").style. display = "block";
+			document.getElementById("box6-simple-content2").style. display = "none";
+			document.getElementById("box6-simple-content3").style. display = "none";
+
+			document.getElementById("box6-simple-button1").style.backgroundColor ="#ddd"
+			document.getElementById("box6-simple-button2").style.backgroundColor ="#BBB"
+			document.getElementById("box6-simple-button3").style.backgroundColor ="#BBB"
+
+			document.getElementById("box6-simple-button1").style.borderBottom ="none"
+			document.getElementById("box6-simple-button2").style.borderBottom ="solid #000 4px"
+			document.getElementById("box6-simple-button3").style.borderBottom ="solid #000 4px"
+		}
+		else if (x == "box6-simple-button2") {
+			document.getElementById("box6-more1").style.display = "none";
+			document.getElementById("box6-more2").style.display = "block";
+			document.getElementById("box6-more3").style.display = "none";
+
+			document.getElementById("box6-simple-content1").style. display = "none";
+			document.getElementById("box6-simple-content2").style. display = "block";
+			document.getElementById("box6-simple-content3").style. display = "none";
+
+
+			document.getElementById("box6-simple-button1").style.backgroundColor ="#BBB"
+			document.getElementById("box6-simple-button2").style.backgroundColor ="#ddd"
+			document.getElementById("box6-simple-button3").style.backgroundColor ="#BBB"
+
+			document.getElementById("box6-simple-button1").style.borderBottom ="solid #000 4px"
+			document.getElementById("box6-simple-button2").style.borderBottom ="none"
+			document.getElementById("box6-simple-button3").style.borderBottom ="solid #000 4px"
+		}
+		else if (x == "box6-simple-button3") {
+			document.getElementById("box6-more1").style.display = "none";
+			document.getElementById("box6-more2").style.display = "none";
+			document.getElementById("box6-more3").style.display = "block";
+
+			document.getElementById("box6-simple-content1").style. display = "none";
+			document.getElementById("box6-simple-content2").style. display = "none";
+			document.getElementById("box6-simple-content3").style. display = "block";
+
+			document.getElementById("box6-simple-button1").style.backgroundColor ="#BBB"
+			document.getElementById("box6-simple-button2").style.backgroundColor ="#BBB"
+			document.getElementById("box6-simple-button3").style.backgroundColor ="#ddd"
+
+
+			document.getElementById("box6-simple-button1").style.borderBottom ="solid #000 4px"
+			document.getElementById("box6-simple-button2").style.borderBottom ="solid #000 4px"
+			document.getElementById("box6-simple-button3").style.borderBottom ="none"
+		}
+
+	})
+})
